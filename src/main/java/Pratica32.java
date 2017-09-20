6b2f5ea7f62e321ca5b25a0dee8bfd3e677ac1c1
Woich <minewoichbr@gmail.com>
2017-09-19 21:54:20 -0300

    Commit Final

--- src/main/java/Pratica32.java
+++ src/main/java/Pratica32.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +11,17 @@
  * @author woich
  */
 public class Pratica32 {
-    public static void main(String[] args){
     
+    public static double densidade(double x, double media, double desvio) {
+        double d = (1.00/(Math.sqrt(2.00 * Math.PI) * desvio)) * (Math.pow(Math.E,(-(1.00/2.00) * Math.pow(((x-media)/desvio), 2))));
+        return d;
+    }
+    
+    public static void main(String[] args){
+        Double densi;
+        
+        densi = densidade(-1.00, 67.00, 3.00);
+        
+        System.out.println(densi);
     }
 }
